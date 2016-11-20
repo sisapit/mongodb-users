569b10ca7a1c3bdb2dc680559ae9cf35ef6ec251
Sebastian Scheffler <sebastian@ipatsch.speedport_w_724v_typ_a_05011603_00_009>
2016-11-20 23:15:23 +0100

    Added first draft of user model

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/users_test');
 mongoose.connection
-    .once('open', () => console.log('Good to go!'))
+    .once('open', () => console.log('APP-I-GOOD, Good to go!'))
     .on('error', (error) => {
-        console.warn('Error', error);
+        console.error('APP-E-FAILED', error);
     });
-    
