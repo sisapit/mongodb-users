11c6832cbab4c1a216fbf1def8a0d911dcc5f7ef
Sebastian <seb.scheffler@gmx.de>
2016-11-22 01:14:18 +0100

    Made sure that connection to MongoDB is established before first test is run.

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,16 +1,34 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/users_test');
-mongoose.connection
-    .once('open', () => console.log('APP-I-GOOD, Good to go!'))
-    .on('error', (error) => {
-        console.error('APP-E-FAILED', error);
-    });
+// Let Mongoose use the ES6 promise implementation.
+mongoose.Promise = global.Promise;
+
+before((done) => {
+    mongoose.connect('mongodb://localhost/users_test');
+
+    // Note: Establishing a connection to MongoDB can take some time. So, take
+    // care to wait until the connection is established and run the tests only then.
+    mongoose.connection
+        .once('open', () => {
+            // Wait until the connection is open and only then call 
+            // the done callback in order to run the first test case.            
+            console.log('NOSQL-I-OPEN, Connection established');
+            done();
+        })
+        .on('error', (error) => {
+            console.error('DB-E-FAILED', error);
+        });
+});
 
 beforeEach((done) => {
-    console.info('TEST-I-DONE');
+    console.info('TEST-I-BEFORE, Before test case callback');
     mongoose.connection.collections.users.drop(() => {
         // Ready to run the next test!
         done();
     });
+});
+
+afterEach((done) => {
+    console.info('TEST-I-AFTER, After test case callback');
+    done();
 });
